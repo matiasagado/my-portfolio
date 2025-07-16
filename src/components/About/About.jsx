7aef93fb9e97fa066400c9db363033b791fa39a0
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2025-07-16 16:05:35 -0700

    About.jsx: add jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,42 @@
 import React from "react";
-import "About.css";
+import "./Aboout.css";
 
 function About() {
+  const one = (
+    <p>
+      I'm currently a <b>Senior</b> at the{" "}
+      <a href="https://www.usfca.edu/">University of San Francisco</a>, pursuing
+      a <b>Bachelor's of Science</b> in <b>Computer Science</b>. I'm passionate
+      about Software Engineering and always eage to explore new technologies.
+    </p>
+  );
+
+  const two = (
+    <p>
+      Outside of coursework I am interested in following the developments of
+      computing technology.
+    </p>
+  );
+
+  const techstack = ["Python", "HTML/CSS", "React.js"];
+
   return (
     <section className="section" id="about">
       <div className="section-header">
         <span className="section-title">About Me</span>
       </div>
       <div className="about-content">
-        <div className="about-description"></div>
+        <div className="about-description">
+          {one}
+          <p>Here are some technologies I've been working with:</p>
+          <ul className="tech-stack">
+            {techstack.map((techitem, i) => (
+              <li key={i}>{techitem}</li>
+            ))}
+          </ul>
+          {two}
+        </div>
+        <img src="./src/assets/tmp/tmp.jpeg" alt="Temporary" />
       </div>
     </section>
   );
