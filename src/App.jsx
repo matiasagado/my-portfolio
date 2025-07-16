d3a0e3d7b3694efc7e3313a4f1eaac0feb4aea62
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2025-07-16 15:34:26 -0700

    add About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <main className="body">
         <Hero />
+        <About />
       </main>
     </div>
   );
---
+++ src/components/About/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "About.css";
+
+function About() {
+  return (
+    <section className="section" id="about">
+      <div className="section-header">
+        <span className="section-title">About Me</span>
+      </div>
+      <div className="about-content">
+        <div className="about-description"></div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
