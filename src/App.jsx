302d15803cc6b315730faf05e69b652349e0689c
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2025-07-16 13:28:36 -0700

    add hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main className="body">
+        <Hero />
+      </main>
     </div>
   );
 }
