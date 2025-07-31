915d169f43eb89a28552b1af8d1b33938902a2bf
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2025-07-30 23:07:07 -0700

    Contact.jsx: store social links

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,28 @@
 import "./Contact.css";
 
 function Contact() {
+  const socialLinks = [
+    {
+      name: "Github",
+      icon: "fab fa-github",
+      link: "https://github.com/matiasagado",
+    },
+    {
+      name: "LinkedIn",
+      icon: "fab fa-linkedin",
+      link: "https://linkedin.com/in/matiasagado",
+    },
+    {
+      name: "Twitter",
+      icon: "fab fa-twitter",
+      url: "https://twitter.com/matt_agado",
+    },
+    {
+      name: "Email",
+      icon: "fas fa-envelope",
+      url: "mailto:mfagado@usfca.edu",
+    },
+  ];
   return (
     <section className="section" id="contact">
       <div className="section-header">
