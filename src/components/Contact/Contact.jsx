5bed3ed130307687725d7665a53fe7f4a8d4e042
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2025-08-01 00:24:58 -0700

    Contact.jsx: add get in touch section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -28,6 +28,25 @@ function Contact() {
       <div className="section-header">
         <div className="section-title">contact me_</div>
       </div>
+      <div className="contact-container">
+        <div className="contact-info">
+          <h3>Get In Touch</h3>
+          <p>
+            I'm currenlty looking for new opportunities. Whether you have a
+            question or want to get in touch, I'll try my best to get back to
+            you.
+          </p>
+        </div>
+        <div className="contact-form-container">
+          <div className="form-success">
+            <h3>Thank You!</h3>
+            <p>
+              Your message have been successfully sent! I will get back to you
+              soon.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
